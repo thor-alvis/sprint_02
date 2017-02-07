65820dcf42df0202278260aebdffe67ee07b4c50
dakotah walker <walker.dakotah@gmail.com>
2017-02-06 19:19:58 -0800

    gets initial oauth login working

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Home'})
-})
+  res.render('index', {title: 'Home'});
+});
 
-module.exports = router
+module.exports = router;
---
+++ routes/profile.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const user = req.session.user;
+  console.log(user)
+  res.render('profile', {user});
+});
+
+router.get('/me', (req, res, next) => {
+  const access_token = req.session.access_token;
+  const url = "https://www.googleapis.com/plus/v1/people/me";
+  const options = {
+    method: 'GET',
+    url: url,
+    headers: { 'authorization' : `Bearer ${access_token}`}
+  }
+  // console.log('getting the profile')
+  request(options, (err, response, body) => {
+    const user = JSON.parse(body);
+    req.session.user = user;
+    return res.redirect('/profile')
+  })
+});
+
+module.exports = router;
